2526bde00ce121226a5df35736952caa891c7b12
Emuekhare King-David Imoghena <emuekharekingdavid@gmail.com>
2026-02-19 13:51:00 +0100

    backend call changes

--- static/app.js
+++ static/app.js
@@ -1,6 +1,6 @@
 function qs(id){ return document.getElementById(id); }
 
-const API_BASE = window.API_BASE || "https://backend-oaaq.onrender.com";
+const API_BASE ="https://backend-oaaq.onrender.com";
 
 async function uploadFile() {
   const fileInput = qs("fileInput");
